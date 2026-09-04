2f6d5a4ab1949221ef1f93f214e2e003fbc7098f
Matt Brown <mtbrown89@hotmail.com>
2026-09-04 00:08:09 -0400

    Trying staticwebapp config

--- src/index.d.ts
+++ src/index.d.ts
@@ -10,3 +10,11 @@ declare module '*.pdf' {
     const value: any;
     export = value;
 };
+declare module '*.png' {
+    const value: any;
+    export = value;
+};
+declare module '*.webp' {
+    const value: any;
+    export = value;
+};
